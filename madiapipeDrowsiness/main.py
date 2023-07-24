e880ee2fa413e691003fbefc283ea76a013f1c81
Sjarelquint <108517712+Sjarelquint@users.noreply.github.com>
2023-07-24 11:58:03 +0300

    Update main.py

--- madiapipeDrowsiness/main.py
+++ madiapipeDrowsiness/main.py
@@ -43,9 +43,3 @@ while cap.isOpened():
         pass
 
     frame_placeholder.image(annotated_image, channels="BGR")
-    #
-    #
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
